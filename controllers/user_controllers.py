d19dd16049056aa2996f4d1125923b108a9e4729
beaalmeidas <beatrizalmeidabss@gmail.com>
2025-02-22 15:22:27 -0300

    ✨ feat: adicionando validação na função de listar todos os usuários

--- controllers/user_controllers.py
+++ controllers/user_controllers.py
@@ -9,16 +9,18 @@ def create_user():
     user_data = request.json
 
     if not user_data.get('email'):
-        return jsonify({'message': 'O e-mail é obrigatório'}), 400
+        return jsonify({'message': 'É obrigatório inserir um email'}), 400
     if not user_data.get('password'):
-        return jsonify({'message': 'A senha é obrigatória'}), 400
+        return jsonify({'message': 'É obrigatório inserir uma senha'}), 400
     if not user_data.get('username'):
-        return jsonify({'message': 'O nome de usuário é obrigatório'}), 400
+        return jsonify({'message': 'É obrigatório inserir um username'}), 400
 
     new_user = User(user_email=user_data['user_email'], 
-                    user_password_hash=user_data['user_password_hash'],
+                    user_password_hash=user_data['password'],
                     username=user_data['username'],
-                    is_admin=user_data['is_admin'])
+                    is_admin=user_data['is_admin', False])
+    
+    new_user.secure_password(user_data['password'])
     
     db.session.add(new_user)
     db.session.commit()
@@ -29,6 +31,10 @@ def create_user():
 @app.route('/users', methods=['GET'])
 def list_users():
     users = User.query.all()
+
+    if not users:
+        return jsonify({"message": "Não há usuários cadastrados :("}), 200
+    
     users_list = [{'id': user.id, 'username': user.username,} for user in users]
 
     return jsonify({'users': users_list})
@@ -42,3 +48,5 @@ def get_user(id):
         return jsonify({'message': 'Usuário não encontrado :('}), 404
     
     return jsonify({'id': searched_user.id, 'username': searched_user.username})
+
+
